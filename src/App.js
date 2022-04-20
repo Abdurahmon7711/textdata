0427e1bd04573bfb1286cb362ea2ebc247b6f0f0
01werka <toshpolatovsherzod1@gmail.com>
2022-04-20 17:55:08 +0500

    werka 17:55

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
- import React from "react";
+import React from "react";
 
-import AdvancedSearch from "./pages/chooseGits/AdvancedSearch/AdvancedSearch";
-import ChooseGits from "./pages/chooseGits/ChooseGits";
 import Main from "./pages/main/Main";
 import Application from "./pages/Application/Application"
 import AboutApp from "./pages/Application/Apllication about/AboutApp"
 import Type from "./pages/Application/Type/Type"
- function App() {
+function App() {
   return (
     <div className="App">
-       {/* <ChooseGits/>
+      {/* <ChooseGits/>
        <Main/> */}
-       <Application/>
-       <br/>
-       <br/>
-       <AboutApp/>
-       <br/>
-       <br/>
-       <Type/>
-       <ChooseGits/>
-       <Main/>
-       <AdvancedSearch/>
+      <Application />
+      <br />
+      <br />
+      <AboutApp />
+      <br />
+      <br />
+      <Type />
+      <Main />
     </div>
   );
 }
