d7b30ae9f826a8299195287b36e7e8cf5b12d5be
makhmud1108 <makhmudnamozov2002@gmail.com>
2022-05-11 18:39:29 +0500

    Azizjon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Bukhara from "./pages/ChineBlog/Bukhara";
+import Xitoy from "./pages/ChineBlog/Xitoy";
 import MainPage from "./pages/main/MainPage"
 import Routs from "./routs/routs";
 
@@ -6,12 +8,17 @@ import Routs from "./routs/routs";
 
 function App() {
   return (
-    <Routs>
-       <div className="App">
-          <MainPage/>
-       </div>
-    </Routs>
-   
+    // <Routs>
+    //    <div className="App">
+    //       {/* <MainPage/> */}
+        
+    //    </div>
+    // </Routs>
+    <div>
+      <Bukhara/>
+      {/* <Blog/> */}
+      <Xitoy/>
+    </div>
   );
 }
 
