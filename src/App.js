8c2bd26c44ac669183da4680faf1870ecb492089
Abdurahmon7711 <a.sulaymonv1177@gmail.com>
2022-05-23 21:32:32 +0500

    fksfsoso

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import React from "react";
-import Authorization from "./pages/authorization/authorization/Authorization";
-import MainPage from "./pages/main/MainPage"
-import Application from "./pages/Application/Application"
-import AboutApp from "./pages/Application/Apllication about/AboutApp"
-import Blog from "./pages/ChineBlog/Blog";
-
+ 
  
 function App() {
   return (
     <div className="App">
-      {/* <Authorization/> */}
-      {/* <MainPage/> */}
-      {/* <Application/> */}
-      <br/>
-      <br/>
-      <hr/>
-      <br/>
-      <br/>
-      {/* <AboutApp/> */}
-
-      <Blog/>
+        sadfasd
     </div>
   );
 }
